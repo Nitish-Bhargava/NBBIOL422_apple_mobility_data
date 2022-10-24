6e6c377a9d1871f5cb707cbb67845dce2d4fc65c
Nitish Bhargava <2001nitish@gmail.com>
2022-10-24 14:13:08 -0700

    delete output files and add .gitkeep

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -1,8 +1,9 @@
-#!/bin/ban
+#!/bin/bash
 
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
 
 Rscript -e "rmarkdown::render('Analysis.Rmd',\
             params = list(state = 'Ohio',\
-            data = 'data/raw_data/applemobilitytrends-2022-04-12.csv))"
+            data = 'data/raw_data/applemobilitytrends-2022-04-12.csv'))"
+
