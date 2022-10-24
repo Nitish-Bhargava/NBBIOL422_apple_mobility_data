3e90076b93055e52380d0f896d6bf25848cb936a
Nitish Bhargava <2001nitish@gmail.com>
2022-10-24 14:03:05 -0700

    First commit of bash render script

---
+++ run_analyze_covid_pipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/ban
+
+# A bash script to drive the rendering of an Rmarkdown file using some
+# parameters passed in from the shell
+
+Rscript -e "rmarkdown::render('Analysis.Rmd',\
+params"
