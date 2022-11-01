a421cc59cf34da65ba19942c28a69a5a3bdef19f
Nitish Bhargava <2001nitish@gmail.com>
2022-11-01 02:17:09 +0000

    Correction of script file and output

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,11 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
 # parsing SARS-Cov-2 sequencing data
 # Nitish Bhargava
 # October 29, 2022
 # 2001nitish@gmail.com
 
-if [ $# -eq "0"]
+if [ $# -eq "0" ]
 then
         echo "To run this script, supply one argument"
         echo "The file to be analyzed."
@@ -14,7 +14,7 @@ fi
 
 # count up the number of sequences in a fasta file
 echo "number of sequences in the file is:"
-bioawk -c fasta '{print ">" $name $comment}' "$1" | wc -1
+bioawk -c fastx '{print ">" $name $comment}' "$1" | wc -l
 
 # Tally up numer of SARS-CoV-2 sequences in dataset from each country and sort output
 # from largest to smallest
