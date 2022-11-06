20a16e4bfe88cb78141a499e2bf66a10e84d69f6
Nitish Bhargava <2001nitish@gmail.com>
2022-11-06 06:06:42 +0000

    correction of grammer mistakes

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -12,7 +12,7 @@
 
 if [ $# -lt 1 ]
 then
-  echo "To run this script, supply 'two argument':"
+  echo "To run this script, supply two arguments:"
   echo "The name of the state to be analyzed and data file path."
   exit 1
 fi
